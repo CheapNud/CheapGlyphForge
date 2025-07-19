4806acb5bdf6bdbf926588a99b6d15a8bfa17416
CheapNud <brechtvandeninden@hotmail.com>
2025-07-19 04:34:50 +0200

    Add project files.

---
+++ CheapGlyphForge.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CheapGlyphForge.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
